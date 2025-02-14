84a3b95480dfa8f272f8298aa148961111d866b4
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2025-02-14 17:24:05 +0100

    ok

--- utils/LeadSystemFunctions.js
+++ utils/LeadSystemFunctions.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function inviaDatiErroreChiamata(numeroTelefono, motivoErrore, type) {
   try {
-    const response = await axios.post(type == "bluedental" ? 'https://leadsystembluedental-production.up.railway.app/api/webhook-elevenlabs-sql-errore-chiamata' : 'https://leadsystemdentistaitalia-production.up.railway.app/api/webhook-elevenlabs-errore-chiamata', {
+    const response = await axios.post(type == "bluedental" ? 'https://leadsystembluedental-production.up.railway.app/api/webhook-elevenlabs-sql-errore-chiamata' : 'https://leadsystembluedental-production.up.railway.app/api/webhook-elevenlabs-errore-chiamata', {
       Numero_Telefono: numeroTelefono,
       Motivo_Errore: motivoErrore
     });
